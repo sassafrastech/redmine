dc29f0d1b3e99f4c71e7d5a1adfa170bca5f15a3
Adam Konner <linesarefuzzy@gmail.com>
2017-08-11 17:26:04 -0400

    sort/group by parent task
    https://www.redmine.org/issues/5325

--- app/models/issue_query.rb
+++ app/models/issue_query.rb
@@ -29,7 +29,7 @@ class IssueQuery < Query
     QueryColumn.new(:tracker, :sortable => "#{Tracker.table_name}.position", :groupable => true),
     QueryColumn.new(:parent,
                     :sortable => ["#{Issue.table_name}.root_id", "#{Issue.table_name}.lft ASC"],
-                    :default_order => 'desc', :caption => :field_parent_issue),
+                    :default_order => 'desc', :caption => :field_parent_issue, :groupable => true),
     QueryAssociationColumn.new(:parent, :subject, :caption => :field_parent_issue_subject),
     QueryColumn.new(:status, :sortable => "#{IssueStatus.table_name}.position", :groupable => true),
     QueryColumn.new(:priority, :sortable => "#{IssuePriority.table_name}.position",
@@ -369,7 +369,7 @@ class IssueQuery < Query
   end
 
   def default_sort_criteria
-    [['id', 'desc']]
+    [['root_id', 'desc'], ['id', 'desc']]
   end
 
   def base_scope
